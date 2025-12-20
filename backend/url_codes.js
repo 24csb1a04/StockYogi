8d4cd14b6466d179c961dc4319f47666059a7c8c
Aditya Shrivastava <as24csb1a04@student.nitw.ac.in>
2025-12-20 22:17:41 +0530

    Add files via upload

---
+++ backend/url_codes.js
@@ -0,0 +1,12 @@
+const urlcodes =  {
+    "NIFTY" : "nifty-50-9",
+    "NIFTY50" : "nifty-50-9",
+    "NIFTYBANK" : "nifty-bank-23",
+    "NIFTYMIDCAP" : "nifty-midcap-100-27",
+    "NIFTYMIDCAP100" : "nifty-midcap-100-27",
+    "NIFTYNEXT50" : "nifty-next-50-6",
+    "NIFTY100" : "nifty-100-28",
+    "SENSEX"  : "sensex-4",
+    "BSESENSEX" : "sensex-4"
+};
+export default urlcodes;
